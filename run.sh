00f9ffd845aec8ae50528d7475bced5433a99b7e
Ken Robertson <ken@invalidlogic.com>
2017-04-04 13:30:24 -0700

    Change directory for session files

--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-mkdir -p /torrents/.session
-
 sed -i -e "s|<FLOOD_SECRET>|$FLOOD_SECRET|g" /usr/flood/config.js
 sed -i -e "s|<CONTEXT_PATH>|$CONTEXT_PATH|g" /usr/flood/config.js
 sed -i -e "s|<SEED_PORT>|$SEED_PORT|g" /home/torrent/.rtorrent.rc
 
-rm -f /torrents/.session/rtorrent.lock
+rm -f /torrents/rtorrent.lock
 mv /usr/flood /usr/fix && mv /usr/fix /usr/flood # fix strange bug
 chown -R $UID:$GID /home/torrent /torrents /tmp /flood-db /etc/s6.d
 
